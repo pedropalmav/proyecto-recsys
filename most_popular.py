c964e29d8c394a8dae787aec9209a2c1acecd033
Pedro Palma <pedro.palma@uc.cl>
2024-10-14 14:21:35 -0300

    feat: Add MostPopular model

---
+++ most_popular.py
@@ -0,0 +1,33 @@
+import os
+import json
+from functools import reduce
+
+class MostPopular:
+    def __init__(self, data):
+        self.data = data
+
+    def fit(self):
+        all_interactions = [user[id]["history_interaction"] for user in self.data for id in user.keys()]
+        interations_count = {}
+        for user_interactions in all_interactions:
+            for interaction in user_interactions:
+                if interaction in interations_count:
+                    interations_count[interaction] += 1
+                else:
+                    interations_count[interaction] = 1
+
+        self.popular = max(interations_count, key=interations_count.get)
+
+    def recommend(self, user_id):
+        return self.popular
+    
+
+if __name__ == "__main__":
+    data = []
+    with open(os.path.join("data", "Books", "final_data.jsonl"), "r") as f:
+        for line in f:
+            data.append(json.loads(line))
+
+    model = MostPopular(data)
+    model.fit()
+    print(model.recommend(1))
